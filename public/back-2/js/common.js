25eab681600f3e742ce8ed4d4988fabd41376e7f
ligang <510118234@qq.com>
2018-10-31 19:05:14 +0800

    首页二级导航，左侧菜单，退出功能完成

--- public/back-2/js/common.js
+++ public/back-2/js/common.js
@@ -9,10 +9,30 @@ $(document).ajaxStop(function () {
 
 $(function () {
   //二级导航的切换
-  // $('.lt_aside .nav .category').click(function () {
-  //   $(this).next().slideToggle();
-  // });
+  $('.lt_aside .nav .category').click(function () {
+    $(this).next().slideToggle();
+  });
   //左侧菜单的切换
-  
+  $('.lt_topbar .icon_menu').click(function () {
+  $('.lt_aside').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+    $('.lt_topbar').toggleClass('hidemenu');
+  });
   //退出功能
+  $('.lt_topbar .icon_logout').click(function () {
+    $('#logoutModal').modal('show');
+  });
+  $('#logoutBtn').click(function () {
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:'json',
+      success:function (info) {
+        console.log(info);
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    })
+  })
 })
